6471a3cd2a730d8fac170b3cf166b0ee828113e3
Solange <solangegrima8@gmail.com>
2026-08-03 20:59:28 -0300

    Realizamos la tercera y cuarta prueba

---
+++ Dsw2026Tpi.Tests/Services/AppointmentServiceGetByPatientDniTests.cs
@@ -0,0 +1,37 @@
+﻿using Dsw2026Tpi.Application.Services;
+using Dsw2026Tpi.CrossCutting.Exceptions;
+using Dsw2026Tpi.Domain.Entities;
+using Dsw2026Tpi.Domain.Interfaces;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Tpi.Tests.Services
+{
+    public class AppointmentServiceGetByPatientDniTests
+    {
+        private readonly IPersistence _persistence;
+        private readonly ILogger<AppointmentService> _logger;
+        private readonly AppointmentService _service;
+
+        public AppointmentServiceGetByPatientDniTests()
+        {
+            _persistence = Substitute.For<IPersistence>();
+            _logger = Substitute.For<ILogger<AppointmentService>>();
+            _service = new AppointmentService(_persistence, _logger);
+        }
+
+        [Fact]
+        public async Task ObtenerPorDni_CuandoElPacienteNoExiste_EntoncesLanzaExcepcionDeNoEncontrado()
+        {
+            long dniInexistente = 30123456;
+
+            _persistence.First<Patient>(Arg.Any<System.Linq.Expressions.Expression<Func<Patient, bool>>>())
+                .Returns((Patient?)null);
+
+            await Assert.ThrowsAsync<EntityNotFoundException>(() => _service.GetByPatientDni(dniInexistente));
+        }
+    }
+}
---
+++ Dsw2026Tpi.Tests/Services/DoctorServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dsw2026Tpi.Application.Services;
+using Dsw2026Tpi.CrossCutting.Exceptions;
+using Dsw2026Tpi.Domain.Entities;
+using Dsw2026Tpi.Domain.Interfaces;
+using NSubstitute;
+using Xunit;
+
+namespace Dsw2026Tpi.Tests.Services
+{
+    public class DoctorServiceTests
+    {
+        private readonly IPersistence _persistence;
+        private readonly DoctorService _service;
+
+        public DoctorServiceTests()
+        {
+            _persistence = Substitute.For<IPersistence>();
+            _service = new DoctorService(_persistence);
+        }
+        [Fact]
+        public async Task Eliminar_CuandoElMedicoExiste_EntoncesLoMarcaComoEliminado()
+        {
+            var speciality = new Speciality("Cardiología", "Especialidad del corazón");
+            var doctor = new Doctor("Dr. Juan Pérez", "MP12345", speciality);
+
+            _persistence.GetById<Doctor>(doctor.Id).Returns(doctor);
+
+            await _service.Delete(doctor.Id);
+
+            Assert.True(doctor.Deleted);
+            await _persistence.Received(1).Update(doctor);
+        }
+    }
+}
